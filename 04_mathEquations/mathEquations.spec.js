f180ff00d368058691d579a43635fb810807a0ee
Rob Hawkinse <rob.hawkins.89@gmail.com>
2025-05-27 12:15:17 +0100

    EX-04 - test 4 - rmv skip. change const d to = 5 % 4 -- TEST PASSES

--- 04_mathEquations/mathEquations.spec.js
+++ 04_mathEquations/mathEquations.spec.js
@@ -4,13 +4,13 @@ describe('math', () => {
   test('a is 0', () => {
     expect(a).toEqual(0);
   });
-  test.skip('b is 9', () => {
+  test('b is 9', () => {
     expect(b).toEqual(9);
   });
-  test.skip('c is 66', () => {
+  test('c is 66', () => {
     expect(c).toEqual(66);
   });
-  test.skip('d is 1', () => {
+  test('d is 1', () => {
     expect(d).toEqual(1);
   });
   test.skip('e is -8', () => {
