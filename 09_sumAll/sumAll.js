f30c3b038a38939d797dd895cf1ff5cb47c07bf7
Rob Hawkinse <rob.hawkins.89@gmail.com>
2025-05-29 16:20:29 +0100

    Ex 09 Sum All - complete - passes tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function( a , b ) {
+let start = 0
+let end = 0
+let newSum = 0;
 
+//  ERROR FOR NEGATIVE NUMBERS  //
+if ( a < 0) return "ERROR"; 
+if ( b < 0) return "ERROR";
+
+// ERROR FOR NON-INTEGER //
+if (!Number.isInteger(a)) return "ERROR"; 
+if (!Number.isInteger(b)) return "ERROR"; 
+
+// ERROR FOR NON-NUMBER PARAMETERS //
+
+
+if ( a < b ){
+    start = a ; end = b ;
+} else {
+    start = b ; end = a ;
+}
+
+for (let i = start; i <= end; i++){
+    newSum += i;  }
+    return newSum
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
