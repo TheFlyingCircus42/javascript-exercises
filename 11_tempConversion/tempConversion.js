7cc07f871c44af75ea9efdb68aadb1d7d3257c6e
Rob Hawkinse <rob.hawkins.89@gmail.com>
2025-11-24 10:25:46 +0000

    Leap years completed - change rounding method to prevent returning a string instead of a number

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+  
+  const convertToCelsius = function (x) {
+  let farToCel = (x - 32) / (9 / 5);
+  // return farToCel.toFixed(1);   //<<<original version
+  return (Math.round(farToCel * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (y) {
+  let celToFar = y * (9 / 5) + 32;
+  // return celToFar.toFixed(1);  //<<< original version
+  return(Math.round(celToFar*10)/10);
 };
+
+
+// console.log(convertToCelsius(32)); 
+// console.log(convertToFahrenheit(20)); 
+
+
+
 
 // Do not edit below this line
 module.exports = {
