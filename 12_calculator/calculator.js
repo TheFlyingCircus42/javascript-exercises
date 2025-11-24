e5392eab71eaf63b8ec44cd3a593cb40007cbec3
Rob Hawkinse <rob.hawkins.89@gmail.com>
2025-11-24 16:33:42 +0000

    ex 12 - calculator complete

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,57 @@
-const add = function() {
-	
+const add = function( a , b ) {
+	return a + b;
+};
+// console.log('ADD FUNTION');
+// console.log(add(1,2));
+
+const subtract = function( c , d ) {
+	return c - d ;
+};
+// console.log('SUBTRACT FUNCTION');
+// console.log(subtract(5,3));
+
+
+const sum = function(sumNumbers) {
+let sumtTally = 0 ;
+for ( let i = 0 ; i < sumNumbers.length ; i++){
+  sumtTally = sumtTally + sumNumbers[i];
+}
+return sumtTally ;
+};
+//console.log('sum function');
+//console.log(sum([1,2,3]))
+
+
+const multiply = function(numbers) {
+let tally = 1 ;
+for (let i = 0 ; i < numbers.length ; i++){
+  tally = tally*numbers[i];
+}
+return tally ;
 };
 
-const subtract = function() {
-	
-};
+// console.log('Multiply Function')
+// console.log(multiply(2, 4, 6, 8, 10, 12, 14))
 
-const sum = function() {
-	
+const power = function(x , y) {
+let powerOf = 1
+for (let i = 0 ; i < y ; i++){
+  powerOf = powerOf*x;
+}	
+return powerOf;
 };
+// console.log(power(2,4))
 
-const multiply = function() {
-
+const factorial = function(n) {
+if (n === 0 || n === 1){return 1;}
+let result = 1;
+for(let i = 2  ; i <= n ; i++) {
+result *= i ;
+} 
+return result ;
 };
+console.log(factorial(5));
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
